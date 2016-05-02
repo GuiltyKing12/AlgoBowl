46b91222454e6a7ba5bde3b189b739e20ce6683d
GuiltyKing12 <bher@mines.edu>
2016-05-01 21:57:36 -0600

    Code now effectively creates and splits graph evenly while also getting cross edges

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,7 @@
 import java.io.FileReader;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -11,13 +13,16 @@ public class Demo {
 	public UndirectedGraph<Integer, DefaultEdge> graph;
 	private String file = "input.txt";
 	public int num_vertices, num_edges;
-	public Set<Integer> graph1, graph2;
+	public Set<Node> graph1, graph2;
+	Map<Integer, Node> vertices;
 	
  	public void run() {
 		graph = new SimpleGraph<Integer, DefaultEdge>(DefaultEdge.class);
-		graph1 = new HashSet<Integer>();
-		graph2 = new HashSet<Integer>();
+		vertices = new HashMap<Integer, Node>();
+		graph1 = new HashSet<Node>();
+		graph2 = new HashSet<Node>();
 		read();
+		split();
 	}
 	
 	private void read() {
@@ -29,6 +34,7 @@ public class Demo {
 			
 			for(int i = 1; i <= num_vertices; i++) {
 				graph.addVertex(i);
+				vertices.put(i, new Node(i));
 			}
 			
 			while(scanner.hasNextLine()) {
@@ -41,7 +47,39 @@ public class Demo {
 	}
 	
 	private void split() {
-		
+		for(Node node : vertices.values()) {
+			if(graph1.size() < graph2.size()) {
+				node.graph = "1";
+				graph1.add(node);
+			}
+			else {
+				node.graph = "2";
+				graph2.add(node);
+			}
+		}
+		for(DefaultEdge edge : graph.edgeSet()) {
+			int source = graph.getEdgeSource(edge);
+			int target = graph.getEdgeTarget(edge);
+			
+			if(vertices.get(source).graph != vertices.get(target).graph) {
+				vertices.get(source).cross_edge++;
+				vertices.get(target).cross_edge++;
+			}
+		}
+	}
+	
+	private void print() {
+		for(Node node : vertices.values()) {
+			System.out.print(node.toString() + " ");
+		}
+		System.out.println();
+		for(Node node : graph1) {
+			System.out.print(node.toString() + " ");
+		}
+		System.out.println();
+		for(Node node : graph2) {
+			System.out.print(node.toString() + " ");
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -49,5 +87,6 @@ public class Demo {
 		demo.run();
 		System.out.println(demo.num_edges);
 		System.out.println(demo.graph.edgeSet());
+		demo.print();
 	}
 }
